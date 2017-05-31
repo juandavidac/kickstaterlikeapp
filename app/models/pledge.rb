d96ba193b5ff0503797d2c5888529f9d0bb94edd
Juan David Aristizábal <jd.ac@outlook.com>
2017-05-31 17:39:46 -0500

    add methods to project and pledges models

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,11 +4,35 @@ class Pledge < ApplicationRecord
 
   before_validation :generate_uuid!, :on => :create
   validates_presence_of :name, :address, :city, :country, :postal_code, :amount, :user_id
+  after_create :check_if_funded
 
+  def charged?
+    status=="charged"
+  end
+  def failed?
+    status == "failed"
+  end
+
+  def pending?
+    status == "pending"
+  end
+
+  def charged!
+    update(status: "charged")
+  end
+
+  def void!
+    update(status: "void")
+  end
+  
   private
   def generate_uuid!
     begin
       self.uuid= SecureRandom.hex(16)
     end while Pledge.find_by(:uuid => self.uuid).present?
+  end
+
+  def check_if_funded
+    project.funded! if project.total_backed_amount >= project.goal
   end
 end
